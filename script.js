d82b1261417f848a843c4f6286824c405187bd2f
GabrielLG <144732815+gabriellogiudice96@users.noreply.github.com>
2024-05-30 00:04:08 -0300

    Update script.js
    comentarios borrados

--- script.js
+++ script.js
@@ -3,22 +3,13 @@ const day = document.getElementById("day");
 const month = document.getElementById("month"); 
 const year = document.getElementById("year"); 
 
-// const hour = document.getElementById("hour");
-// const min = document.getElementById("minutes");
-// const sec = document.getElementById("seconds");
 
 const today = new Date();
 
 const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const allMonths = ["January", "February", "March",  "April", "May", "June", "July", "August","September","October","November","December"];
 
-// let hours = today.getHours();
-// let minutes = today.getMinutes();
-// let seconds = today.getSeconds();
 
-// hour.innerHTML = hours;
-// sec.innerHTML = (today.getSeconds()<10?"0":"")+ today.getSeconds();
-// min.innerHTML = (today.getMinutes()<10?"0":"")+ today.getMinutes();
 date.innerHTML = (today.getDate()<10?"0":"")+ today.getDate();
 day.innerHTML = weekDays[today.getDay()];
 month.innerHTML = allMonths[today.getMonth()]; 
